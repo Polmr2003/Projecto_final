a6741f9c0c90d8f3c63873b4fb14b0890ea6e894
Polmr2003 <pol.mr.2003@gmail.com>
2024-11-08 11:40:17 +0100

    pol: templates

--- gestion_cv/profile_cv/admin.py
+++ gestion_cv/profile_cv/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Profile)
+admin.site.register(ExperienciaLaboral)
+admin.site.register(FormacionAcademica)
+admin.site.register(HardSkill)
+admin.site.register(SoftSkill)
+admin.site.register(Idioma)
+admin.site.register(Voluntariado)
+admin.site.register(Proyecto)
+admin.site.register(Publicacion)
+admin.site.register(ReconocimientoPremio)
+admin.site.register(CertificacionCurso)
