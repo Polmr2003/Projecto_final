329dad53d2de069318df11c77d08a0f7c6567341
dengrau <montsedegr@gmail.com>
2024-11-08 11:45:10 +0100

    montse: configuradas las urls

--- gestion_cv/profile_cv/urls.py
+++ gestion_cv/profile_cv/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path
-
 urlpatterns = [
-    path("", admin.site.urls),
+    path('users/', views.user_list, name='user_list'),
+    path('users/create/', views.create_user, name='create_user'),
+    path('users/update/<int:user_id>/', views.update_user, name='update_user'),
+    path('users/delete/<int:user_id>/', views.delete_user, name='delete_user'),
 ]
